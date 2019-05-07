9fd08fae9c9537a64dd1d0703014401a287b90f3
naymc <naumenkosergeyns@gmail.com>
2019-05-07 21:39:10 +0300

    sergey переменные создаются, записываются в файл их можно от туда дастовать, и решать, но не везже есть реализация операций если поменять местами операнды, например вектора и скаляра есть, а скаляра и вектора нет, тоже самое с матрицей

--- src/calculator/Patterns.java
+++ src/calculator/Patterns.java
@@ -5,8 +5,8 @@ final class Patterns {
 
     }
 
-//    static final String OPERATIONS = "[-+*/=s]";
-static final String OPERATIONS = "(?<![-+*/={,]|\\s|^)([-+=*/])";
+    static final String OPERATIONS = "[-+*/=s]";
+//    static final String OPERATIONS = "(?<![-+*/={,s]|\\s|^)([-+=*/s])";
     static final String SCALAR = "-?\\d+(.\\d+)?";
     static final String VECTOR = "\\{((-?\\d+(.\\d+)?)(,?\\s*))+}";
     static final String MATRIX = "\\{(\\{((-?\\d+(.\\d+)?)(,?\\s*))+},?\\s*)+}";
